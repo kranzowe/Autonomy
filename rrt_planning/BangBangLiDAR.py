e383066197f8daf5970cc73957ed19bf005da1f8
kranzowe <kranzowe@gmail.com>
2026-04-18 16:05:27 -0600

    less in the back

--- rrt_planning/BangBangLiDAR.py
+++ rrt_planning/BangBangLiDAR.py
@@ -45,11 +45,11 @@ class WallFollower(Node):
         avg_fronts = np.mean(valid_fronts) if valid_fronts else 12.0
         left  = self.get_range_at_angle(msg,  90.0)
         right = self.get_range_at_angle(msg, -90.0)
-        rights = [self.get_range_at_angle(msg, x) for x in np.arange(-40.0, -120.0, -5.0)]
+        rights = [self.get_range_at_angle(msg, x) for x in np.arange(-40.0, -100.0, -5.0)]
         valid_rights = [r for r in rights if np.isfinite(r)]
         avg_right = np.mean(valid_rights) if valid_rights else 12.0
         min_right = np.min(valid_rights)
-        angles = np.arange(-40.0, -120.0, -5.0)
+        angles = np.arange(-40.0, -100.0, -5.0)
         min_angle = angles[np.argmin(rights)] 
         self.get_logger().info(f'front: {front:.2f}  left: {left:.2f}  min_angle: {min_angle:.2f} min_r: {min_right}')
 
