c9444449b9b80e4b6947acd964973a889a2f945e
kranzowe <kranzowe@gmail.com>
2026-04-20 09:38:29 -0600

    new

--- rrt_planning/BangBangLiDAR.py
+++ rrt_planning/BangBangLiDAR.py
@@ -45,11 +45,11 @@ class WallFollower(Node):
         avg_fronts = np.mean(valid_fronts) if valid_fronts else 12.0
         left  = self.get_range_at_angle(msg,  90.0)
         right = self.get_range_at_angle(msg, -90.0)
-        rights = [self.get_range_at_angle(msg, x) for x in np.arange(-50.0, -115.0, -5.0)]
+        rights = [self.get_range_at_angle(msg, x) for x in np.arange(-50.0, -125.0, -5.0)]
         valid_rights = [r for r in rights if np.isfinite(r)]
         avg_right = np.mean(valid_rights) if valid_rights else 12.0
         min_right = np.min(valid_rights)
-        angles = np.arange(-50.0, -115.0, -5.0)
+        angles = np.arange(-50.0, -125.0, -5.0)
         min_angle = angles[np.argmin(rights)] 
         self.get_logger().info(f'front: {front:.2f}  left: {left:.2f}  min_angle: {min_angle:.2f} min_r: {min_right}')
 
@@ -95,7 +95,7 @@ class WallFollower(Node):
             twist.linear.x = DEFAULT_SPEED
             dist_component = -dist_error * 1.0
             angle_component = angle_error * 0.17 # was 0.15
-            deriv_component = -dist_derivative * 0.30
+            deriv_component = -dist_derivative * 0.40
             twist.angular.z = dist_component + angle_component + deriv_component
             
             self.get_logger().info(
