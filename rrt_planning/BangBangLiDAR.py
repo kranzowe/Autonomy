f6efe8a12c9c4daa65b7cca919cbcf5e7247cce9
kranzowe <kranzowe@gmail.com>
2026-04-20 10:16:47 -0600

    ghhgfgf

--- rrt_planning/BangBangLiDAR.py
+++ rrt_planning/BangBangLiDAR.py
@@ -93,7 +93,7 @@ class WallFollower(Node):
 
 
             twist.linear.x = DEFAULT_SPEED
-            dist_component = -dist_error * 15
+            dist_component = -dist_error *8
             angle_component = angle_error * 0.2 # was 0.15
             deriv_component = -dist_derivative * 0.0 #0.40
             twist.angular.z = dist_component + angle_component + deriv_component
