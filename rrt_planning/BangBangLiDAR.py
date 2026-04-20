34033fb05260b0e15673c8c228c5857ad193f3a6
kranzowe <kranzowe@gmail.com>
2026-04-20 10:10:42 -0600

    g';lakjfng

--- rrt_planning/BangBangLiDAR.py
+++ rrt_planning/BangBangLiDAR.py
@@ -6,7 +6,7 @@ from geometry_msgs.msg import Twist
 import numpy as np
 import time
 # Match WASD node defaults
-DEFAULT_SPEED = 0.36
+DEFAULT_SPEED = 0.38
 DEFAULT_TURN_RATE = 7.0
 CENTERING_THRESHOLD = 0.5  # meters before correcting
 CENTER = 0.65
@@ -95,7 +95,7 @@ class WallFollower(Node):
             twist.linear.x = DEFAULT_SPEED
             dist_component = -dist_error * 10
             angle_component = angle_error * 0.2 # was 0.15
-            deriv_component = -dist_derivative * 0.2 #0.40
+            deriv_component = dist_derivative * 0.2 #0.40
             twist.angular.z = dist_component + angle_component + deriv_component
             
             self.get_logger().info(
